1f8c9a3af278c69fe0dd5f794cbb29cf7731f7b9
sugyeong01 <2sugyeong01@gmail.com>
2026-09-19 21:23:38 -0700

    09192026mp4

--- js/script.js
+++ js/script.js
@@ -132,7 +132,7 @@ const projects = {
             },
             {
                 type: "video",
-                src: "video/miuwil.mㅔ4"
+                src: "video/miuwil.mp4"
             },
         
         ]
